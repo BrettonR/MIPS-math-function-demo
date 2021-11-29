5935964530c0738e91a1767803a6f2e0bd1dcde7
Bretton Raymond <braymond3@horizon.csueastbay.edu>
2021-11-29 12:22:01 -0800

    Added comments to main.asm

--- main.asm
+++ main.asm
@@ -1,6 +1,15 @@
-# 
-.data
+# driver file: main.asm
+# this file runs the program menu
+# and calls the required functions
 
+.data
+menu_options: .asciiz "1) Display the stored function/n2) Enter a new function and delete the currently stored functionn\3) Evaluate the stored function for an input\n4) "
 
 .text
 main:
+
+
+
+
+li $v0, 10
+syscall
